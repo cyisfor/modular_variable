43f62f25661f1c5f144eb22aa7a9d31c51ef2239
autocommit <autocommit>
2018-12-31 07:51:02 +0000

    auto 0 80 1183

--- modular_variable.cmake
+++ modular_variable.cmake
@@ -32,52 +32,40 @@ function(modular_variable)
 	set(type "${V_TYPE}")
 	set(init "${V_INIT}")
 
-	set(sources "")
+	set(sources)
 	if(DEFINED V_DEPENDERS)
-		set(DEP "")
+		set(DEP)
 	endif()
 
 	foreach(suffix IN ITEMS h internal.h c)
-		set(output "${MODVARDIR}/${filename}.${suffix}")		
-		set(dirty false)
-		file(LOCK "${output}.lock")
-#		debugvars()
+		set(output "${MODVARDIR}/${filename}.${suffix}")
+		set(inputs)
 		foreach(module IN LISTS V_MODULES)
-			set(input "${CMAKE_CURRENT_SOURCE_DIR}/modvar/${module}.${suffix}.in")
-			if(EXISTS "${input}")
-				configure_file("${input}" "${input}.temp")
-				file(READ "${input}.temp" contents)
-				if(NOT ${dirty})
-					set(dirty true)
-					file(WRITE "${output}.temp" "${contents}")
-				else()
-					file(APPEND "${output}.temp" "${contents}")
-				endif()
-			endif()
+			list(APPEND inputs "${CMAKE_CURRENT_SOURCE_DIR}/modvar/${module}.${suffix}.in")
 		endforeach(module)
-		if(${dirty})
-			file(RENAME "${output}.temp" "${output}")
-			file(REMOVE "${output}.temp")
-			if("${suffix}" STREQUAL "c")
-				list(APPEND sources "${output}")
-				# propagate to parent scope
-				# so we can actually use the generated sources in our projects :p
-				set(${cmakename}_MODVAR_SOURCES "${sources}" PARENT_SCOPE)
-			elseif(DEFINED V_DEPENDERS)
-				list(APPEND DEP "${output}")
-			endif()
-		endif()
-		file(LOCK "${output}.lock" RELEASE)
-		file(REMOVE "${output}.lock")
-	endforeach(suffix)
 
-	if(DEFINED V_DEPENDERS)
-		# have to manually specify dependencies
-		# since cpp can't #include a file that hasn't been generated w/out error
-		foreach(target IN LISTS V_DEPENDERS)
-			get_source_file_property(OLD "${target}" OBJECT_DEPENDS)
-			set(NEW ${OLD} ${DEP})
-			set_source_files_properties("${target}" OBJECT_DEPENDS "${NEW}")
-		endforeach(target)
+		add_custom_command(
+			OUTPUT "${output}"
+			COMMAND cmake
+			  "-DV_MODULES=${V_MODULES}"
+				"-Dinputs=${inputs}"
+				"-Dname=${name}"
+				"-Dfilename=${filename}"
+				"-Dcmakename=${cmakename}"
+				"-Dtype=${type}"
+				"-Dsuffix=${suffix}"
+				"-Doutput=${output}"
+				"-Dmodvar=${CMAKE_CURRENT_SOURCE_DIR}/modvar"
+				-P build_modular_variable.cmake
+				DEPENDS "${inputs}")
+		# then... depend on ${output}?
+		if("${suffix}" STREQUAL "c")
+			list(APPEND sources "${output}")
+			# propagate to parent scope
+			# so we can actually use the generated sources in our projects :p
+		endif()
+	endforeach(suffix)
+	if(NOT "${sources}" STREQUAL "")
+		set(${cmakename}_MODVAR_SOURCES "${sources}" PARENT_SCOPE)
 	endif()
 endfunction(modular_variable)

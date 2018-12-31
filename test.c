9b03fff94e71c944306a69e145fac6e7903d9c39
autocommit <autocommit>
2018-12-31 01:05:27 +0000

    auto 0 23 198

--- test.c
+++ test.c
@@ -1,6 +1,11 @@
 #include "testvar.h"
+#include "testvar.internal.h"
+#include <stdio.h>
+
+int testvar = 3;
+
 int main(int argc, char *argv[])
 {
-	
+	printf("Some stuff from the variables %d %d\n", foo, bar);
 	return 0;
 }

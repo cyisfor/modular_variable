a6db8a7d578482172306cb7077904828a722263a
autocommit <autocommit>
2018-12-31 02:07:05 +0000

    auto 0 61 754

--- modular_variable.cmake
+++ modular_variable.cmake
@@ -1,3 +1,6 @@
+# modular_variable(name stuff) creates ${name}_MODVAR_SOURCES which need to
+# be added as sources to use that variable, if they aren't an empty list.
+
 set(MODVARDIR ${CMAKE_CURRENT_BINARY_DIR}/modvar)
 include_directories(${MODVARDIR})
 # for clarity in including the headers:
@@ -5,37 +8,41 @@ set(MODVARDIR ${MODVARDIR}/modvar)
 # i.e. #include "modvar/something.h"
 file(MAKE_DIRECTORY ${MODVARDIR})
 
-# so we can actually use the generated sources in our projects :p
-set(MODULAR_VARIABLE_SOURCES "" PARENT_SCOPE)
-
 function(modular_variable)
 	cmake_parse_arguments(PARSE_ARGV 1 V
-		"" "TYPE;INIT" "MODULES")
-	set(name ${ARGV0})
-	set(type ${V_TYPE})
-	set(init ${V_INIT})
+		"" "TYPE;INIT;VAR" "MODULES")
+	set(name "${ARGV0}")
+	if(${V_VAR})
+		set(var "${V_VAR}")
+	else()
+		set(var "${name}")
+	set(type "${V_TYPE}")
+	set(init "${V_INIT}")
+
+	set(sources "")
 	foreach(suffix IN ITEMS h internal.h c)
-		set(output ${MODVARDIR}/${name}.${suffix})
+		set(output "${MODVARDIR}/${name}.${suffix}")
 #		debugvars()
-		file(LOCK ${output}.temp)
-		file(WRITE ${output}.temp)
+		file(LOCK "${output}.temp")
+		file(WRITE "${output}.temp")
 		foreach(module IN LISTS V_MODULES)
-			set(input ${CMAKE_CURRENT_SOURCE_DIR}/modvar/${module}.${suffix}.in)
-			if(EXISTS ${input})
-				configure_file(${input} ${input}.temp)
-				file(READ ${input}.temp contents)
-				file(APPEND ${output}.temp "${contents}")
-				file(READ ${output}.temp derp)
+			set(input "${CMAKE_CURRENT_SOURCE_DIR}/modvar/${module}.${suffix}.in")
+			if(EXISTS "${input}")
+				configure_file("${input}" "${input}.temp")
+				file(READ "${input}.temp" contents)
+				file(APPEND "${output}.temp" "${contents}")
 			endif()
 		endforeach(module)
-		if(EXISTS ${output}.temp)
+		if(EXISTS "${output}.temp")
 #			file(REMOVE ${output}.temp1)
-			file(RENAME ${output}.temp ${output})
+			file(RENAME "${output}.temp" "${output}")
 			if("${suffix}" STREQUAL "c")
+				list(APPEND sources "${output}")
+				# propagate to parent scope
 				# so we can actually use the generated sources in our projects :p
-				list(APPEND MODULAR_VARIABLE_SOURCES "${output}")
+				set(${name}_MODVAR_SOURCES "${sources}" PARENT_SCOPE)
 			endif()
 		endif()
-		file(LOCK ${output}.temp RELEASE)
+		file(LOCK "${output}.temp" RELEASE)
 	endforeach(suffix)
 endfunction(modular_variable)
